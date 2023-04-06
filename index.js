97e9f4a2ba4e422a0d651e5f050614daae50d8c1
Teddy <teddywambua5456@gmail.com>
2023-04-06 16:00:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -9,21 +9,31 @@ document.addEventListener('DOMContentLoaded', function() {
   
 	//get movie list that matches the search
 	function getMovieList() {
-	  let searchInputText = document.getElementById('search-bar').value.trim();
-	  const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': 'cd1c5eef93msh44b8ea132228de9p1368ecjsnc67e72425701',
-			'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-		}
-	};
-	
-	fetch(`https://moviesdatabase.p.rapidapi.com/titles/search/keyword/${searchInputText}`, options)
-		.then(response => response.json())
-		.then(data => {
-			console. log(data)
-		})
-		.catch(err => console.error(err));
+		let searchInputText = document.getElementById('search-bar').value.trim();
+		 
+		fetch(`https://api.themoviedb.org/3/search/movie?api_key=b2ef1799992ce938b1ada1c064f8cdb7&language=en-US&query=${searchInputText}&page=1&include_adult=false`)
+			.then(response => response.json())
+			.then(data => {
+				let html = "";
+				if(data.results){
+				  data.results.forEach(data => {
+					html += `
+					  <div class="movie-results">
+						<div class="movie-poster">
+						  <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="${data.title}">
+						</div>
+						<div class="movie-title">
+						  <h3 class="title-text">${data.title}</h3>
+						  <a href="#" class="info-btn">Get More Info</a>
+						</div>
+					  </div>
+					`;
+				  });
+				} else {
+				  html = "No results found";
+				}
+				movieList.innerHTML = html;
+			  })
+			  
 	}
-  });
-  
+});
