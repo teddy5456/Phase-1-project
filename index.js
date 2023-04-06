1e438524d40973aca9b6d09c761b5c57ac58d42d
Teddy <teddywambua5456@gmail.com>
2023-04-06 13:32:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+	const searchBtn = document.getElementById('search-btn');
+	const movieList = document.getElementById('movies');
+	const movieDetails = document.querySelector('.movie-details-content');
+	const infoCloseBtn = document.getElementById('btn-close');
+  
+	//event listeners
+	searchBtn.addEventListener('click', getMovieList);
+  
+	//get movie list that matches the search
+	function getMovieList() {
+	  let searchInputText = document.getElementById('search-bar').value.trim();
+	  const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'cd1c5eef93msh44b8ea132228de9p1368ecjsnc67e72425701',
+			'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+		}
+	};
+	
+	fetch(`https://moviesdatabase.p.rapidapi.com/titles/search/keyword/${searchInputText}`, options)
+		.then(response => response.json())
+		.then(data => {
+			console. log(data)
+		})
+		.catch(err => console.error(err));
+	}
+  });
+  
